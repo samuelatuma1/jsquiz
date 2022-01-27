edd9b971a52db0c21bd47586a36b9b2db64c6d57
samuelatuma1 <atumasaake@gmail.com>
2022-01-27 20:53:29 +0100

    Multiple choice quiz application

---
+++ javascript.js
@@ -0,0 +1,24 @@
+const jsquestions = [
+    {
+        type: 'basic',
+        quest : "Which of the following quotation should you use if you wish to mix JavaScript code with strings",
+        a : "''",
+        b: '""',
+        c: '**',
+        d: '``',
+        ans: 'd'
+    },
+
+    {
+        type: 'basic',
+        quest : "Which of the following is not true about JavaScript?",
+        a : "JavaScript can be both object oriented, functional and/or procedural",
+        b: 'JavaScript is strongly typeed',
+        c: 'JavaScript is dynamically typed',
+        d: 'JavaScript treats empty primitive data as Falsies',
+        ans: 'b'
+    }
+
+]
+
+export default jsquestions;
---
+++ python.js
@@ -0,0 +1,13 @@
+const pyquestions = [
+    {
+        type: 'basic',
+        quest : "Which of the following quotation should you use if you wish to mix Python code with strings",
+        a : "''",
+        b: '""',
+        c: '**',
+        d: 'f""',
+        ans: 'd'
+    }
+]
+
+export default pyquestions;
